55b81d53ad971fd08512463a8870b48a122e962c
alexisgames <alexisgames97@gmail.com>
2022-08-29 09:43:33 -0300

    Entrega MTV terminada

--- MTV/Familiares/views.py
+++ MTV/Familiares/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .models import *
 from django.http import HttpResponse
+from django.template import Context, Template, loader
 
 def familiaresModel(request):
     papa=FamiliaresModel(nombre="Fernando", edad=64, nacimiento='1958-04-25')
@@ -9,9 +10,11 @@ def familiaresModel(request):
     papa.save()
     mama.save()
     hijo.save()
-    return render (request, "vista.html")
-    
-    
-    
-    #texto=f"hola mundo"
-    #return HttpResponse(texto)
+    diccionario={
+                'Familiar_1':(papa.nombre, papa.edad, papa.nacimiento), 
+                'Familiar_2':(mama.nombre, mama.edad, mama.nacimiento),  
+                'Familiar_3':(hijo.nombre, hijo.edad, hijo.nacimiento),
+                }
+    plantilla=loader.get_template("vista.html")
+    documento=plantilla.render(diccionario)
+    return HttpResponse(documento)
